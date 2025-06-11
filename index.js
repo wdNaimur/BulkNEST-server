ca3e8b04abb2d04e64fa0e2778aa8955aede83fb
NaimurWD <naimur.wd@gmail.com>
2025-06-11 04:02:11 -0600

    Implement Firebase JWT authentication and secure product routes with email verification

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const admin = require("firebase-admin");
+const decoded = Buffer.from(
+  process.env.FIREBASE_SERVICE_KEY,
+  "base64"
+).toString("utf8");
+var serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 const port = process.env.PORT || 3000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://bulknest.web.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+// Jwt Middleware
+const verifyJWT = async (req, res, next) => {
+  const token = req?.headers?.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = decoded.email;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  //firebase Verify token firebase admin sdk
+};
 
 const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
@@ -24,24 +58,39 @@ async function run() {
     const orderCollection = database.collection("orders");
 
     // get all product
-    app.get("/products", async (req, res) => {
-      const allProducts = await productCollection
-        .find()
-        .sort({ _id: -1 })
-        .toArray();
-      res.send(allProducts);
+    app.get("/products/:email", verifyJWT, async (req, res) => {
+      const decodedEmail = req.tokenEmail;
+      const email = req.params.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "unauthorized access" });
+      } else {
+        const allProducts = await productCollection
+          .find()
+          .sort({ _id: -1 })
+          .toArray();
+        res.send(allProducts);
+      }
     });
     // get  products by categories
-    app.get("/products/:category", async (req, res) => {
+    app.get("/categories/:category", verifyJWT, async (req, res) => {
+      const decodedEmail = req.tokenEmail; // ✅ From your middleware
+      const requestEmail = req.query.email; // ✅ From query param
       const category = req.params.category;
+
+      // Optional: check if decoded email and query email match
+      if (decodedEmail !== requestEmail) {
+        return res.status(403).send({ message: "Forbidden: Email mismatch" });
+      }
+
       const query = { category: category };
       const product = await productCollection
         .find(query)
         .sort({ _id: -1 })
         .toArray();
-      console.log(product);
+
       res.send(product);
     });
+
     // get single product
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
@@ -77,41 +126,46 @@ async function run() {
       res.send(result);
     });
     // get order data by email my cart
-    app.get("/orders/:email", async (req, res) => {
+    app.get("/orders/:email", verifyJWT, async (req, res) => {
+      const decodedEmail = req.tokenEmail;
       const email = req.params.email;
       const query = { orderedFrom: email };
+      console.log(email, decodedEmail);
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      if (email === decodedEmail) {
+        try {
+          const orders = await orderCollection
+            .find(query)
+            .sort({ _id: -1 })
+            .toArray();
 
-      try {
-        const orders = await orderCollection
-          .find(query)
-          .sort({ _id: -1 })
-          .toArray();
+          if (!orders.length) {
+            return res
+              .status(404)
+              .send({ message: "No orders found for this email." });
+          }
 
-        if (!orders.length) {
-          return res
-            .status(404)
-            .send({ message: "No orders found for this email." });
+          const fullOrderDetails = await Promise.all(
+            orders.map(async (order) => {
+              const productQuery = { _id: new ObjectId(order.productId) };
+              const productDetails = await productCollection.findOne(
+                productQuery
+              );
+              order.productImage = productDetails.image;
+              order.productBrand = productDetails.brand;
+              order.productName = productDetails.name;
+              order.totalPrice = order.quantity * productDetails.price;
+              return order;
+            })
+          );
+
+          res.send(fullOrderDetails);
+        } catch (err) {
+          console.error(err);
+          res.status(500).send({ message: "Error retrieving orders" });
         }
-
-        const fullOrderDetails = await Promise.all(
-          orders.map(async (order) => {
-            const productQuery = { _id: new ObjectId(order.productId) };
-            const productDetails = await productCollection.findOne(
-              productQuery
-            );
-            order.productImage = productDetails.image;
-            order.productBrand = productDetails.brand;
-            order.productName = productDetails.name;
-            order.totalPrice = order.quantity * productDetails.price;
-
-            return order;
-          })
-        );
-
-        res.send(fullOrderDetails);
-      } catch (err) {
-        console.error(err);
-        res.status(500).send({ message: "Error retrieving orders" });
       }
     });
     // Order functionality
